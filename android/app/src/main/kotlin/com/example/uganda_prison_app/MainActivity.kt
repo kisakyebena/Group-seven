85782ee6d01bdce2736274f6f3ee13ab2cb7f4e8
reagannew256-star <reagannew256@gmail.com>
2026-05-08 09:15:03 +0300

    initiate commit

--- android/app/src/main/kotlin/com/example/uganda_prison_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/uganda_prison_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.uganda_prison_fresh
+package com.example.uganda_prison_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ import 'config/firebase_config.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  try {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  } on FirebaseException catch (e) {
+    if (e.code != 'duplicate-app') rethrow;
+  }
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
